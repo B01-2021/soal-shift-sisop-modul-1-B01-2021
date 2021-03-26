a965405242a0712cf51eea70897033230f8df92e
Shidqi Dhaifullah <81344394+shdkong1@users.noreply.github.com>
2021-03-26 22:14:12 +0700

    add cd to current directory

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+parent="/mnt/c/Users/shidqi/sisop1"
+cd $parent
 
 download=`ls -1 | awk 'BEGIN{ x = 0; y = 0 } /^Kucing_/ { ++x } /^Kelinci_/ { ++y } END{ if ( x <= y ) { printf "Kucing_" } else { printf "Kelinci_" }}'`
 datestr=`date +%d-%m-%Y`
