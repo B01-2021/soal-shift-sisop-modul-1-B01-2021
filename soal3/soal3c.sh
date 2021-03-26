227605178d65495cb399604a95ee84e84b0aef90
Shidqi Dhaifullah <81344394+shdkong1@users.noreply.github.com>
2021-03-26 14:37:16 +0700

    download bunny

---
+++ soal3/soal3c.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+x=1
+while [ $x -le 23 ]
+do
+  wget -o /dev/stdout -nv https://loremflickr.com/320/240/bunny | tee -a Foto.log
+  x=$((x+1))
+done
+
+md5sum * | sort | awk 'BEGIN{hash = ""} $1 == hash {print $2} {hash = $1}' | xargs rm
+
+num=1
+for file in *
+do
+  if [[ $file == *"bunny"* ]]
+  then
+    filename=`printf "Koleksi_%02d.jpg" $num`
+    mv $file $filename
+    num=$(($num+1))
+  fi
+done
