4a9e68a1c3fc9cec158465bbeb520b567530b518
Shidqi Dhaifullah <81344394+shdkong1@users.noreply.github.com>
2021-03-27 16:43:07 +0700

    compliance fix

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -2,11 +2,22 @@
 parent="/mnt/c/Users/shidqi/sisop1"
 cd $parent
 
+download=`ls -1 | awk 'BEGIN{ x = 0; y = 0 } /^Kucing_/ { ++x } /^Kelinci_/ { ++y } END{ if ( x <= y ) { printf "Kucing_" } else { printf "Kelinci_" }}'`
+datestr=`date +%d-%m-%Y`
+dirname=$download$datestr
+mkdir $dirname
+
 x=1
 while [ $x -le 23 ]
 do
-  wget -o /dev/stdout -nv https://loremflickr.com/320/240/bunny | tee -a Foto.log
-  x=$((x+1))
+  if [ $download == 'Kucing_' ]
+  then
+    wget -o /dev/stdout -nv https://loremflickr.com/320/240/kitten | tee -a Foto.log
+    x=$((x+1))
+  else
+    wget -o /dev/stdout -nv https://loremflickr.com/320/240/bunny | tee -a Foto.log
+    x=$((x+1))
+  fi
 done
 
 md5sum * | sort | awk 'BEGIN{hash = ""} $1 == hash {print $2} {hash = $1}' | xargs rm
@@ -14,10 +25,18 @@ md5sum * | sort | awk 'BEGIN{hash = ""} $1 == hash {print $2} {hash = $1}' | xar
 num=1
 for file in *
 do
-  if [[ $file == *"bunny"* ]]
+  if [[ $file == *"kitten"* || $file == *"bunny"* ]]
   then
     filename=`printf "Koleksi_%02d.jpg" $num`
     mv $file $filename
     num=$(($num+1))
+  fi
+done
+
+mv Foto.log $dirname
+for file in *
+do
+	@@ -22,5 +13,3 @@ do
+    mv $file $dirname
   fi
 done
