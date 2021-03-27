96bfc22fd32c33003d82298a4df0ec51b70118ee
Shidqi Dhaifullah <81344394+shdkong1@users.noreply.github.com>
2021-03-27 16:30:55 +0700

    compliance fix

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -4,12 +4,6 @@ cd $parent
 
 d=`date +%m%d%Y`
 
-if [ $1 == "-u" ]
-then
-  unzip -P $d Koleksi
-  rm *.zip
-else
-  zip -mr -P $d Koleksi . -i "*.jpg" "*.log"
-  rm -d \Kucing*
-  rm -d \Kelinci*
-fi
+zip -mr -P $d Koleksi . -i "*.jpg" "*.log"
+rm -d \Kucing*
+rm -d \Kelinci*
